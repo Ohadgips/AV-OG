ca1605d7e892d684f172c34b4f185fad8fdafb40
Ohadgips <138259832+Ohadgips@users.noreply.github.com>
2023-12-31 09:31:27 +0200

    Update Behavior.py (#2)

--- Behavior_based_Detection/Behavior.py
+++ Behavior_based_Detection/Behavior.py
@@ -4,4 +4,14 @@ class BehaviorDetection:
         pass
 
 
-    def performance():
+    def bandwithspikes(currentbw):
+        if currentbw => bandwith*2:
+            return "something is sus"
+        return "okay"
+
+    def datastorage(currentdata):
+        if data > currentdata:
+            return "something is sus"
+        else if (data/100)*130 < currentdata:
+            return "something is sus"
+        return "okay"
