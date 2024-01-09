27bf9e3fa7021c1046fb956e586f0d8460934c71
Ohadgips <138259832+Ohadgips@users.noreply.github.com>
2024-01-09 18:21:19 +0200

    Documents Upload

--- Behavior_based_Detection/Behavior.py
+++ Behavior_based_Detection/Behavior.py
@@ -1,7 +1,13 @@
 class BehaviorDetection:
-    
+
+    flag = False
+
     def __init__(self):
         pass
 
 
-    def performance():
+    def numofwindows(self,windows):
+        if windows < 4:
+            return 
+
+    def 
