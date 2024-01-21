16e659a83d71ab95847a660d44826fb525b5d282
Ohadgips <138259832+Ohadgips@users.noreply.github.com>
2024-01-21 22:41:29 +0200

    Process Dataset Part 1

--- Behavior_based_Detection/Behavior_Detection_ML.py
+++ Behavior_based_Detection/Behavior_Detection_ML.py
@@ -1,16 +1,21 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import os
 
 #model
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
-from sklearn.metrics import accuracy_score 
+os.getcwd()
+dataset_path = os.getcwd() +"\Datasets\\DLLs_Imported.csv"
+print(dataset_path)
+dataset = pd.read_csv(dataset_path) # will enter the dataset path here
 
-dataset = pd.read_csv('') # will enter the dataset path here
+print(dataset.head())
 
-#dup removal example
-label = LabelEncoder()
-label.fit(dataset.example.drop_duplicates())
-dataset.example = label.transform(dataset.example)
+print(dataset.isnull().sum())
+
+label_encoder = LabelEncoder()
 
