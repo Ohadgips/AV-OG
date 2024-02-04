cfea4f17ec61146c00325767f19025f56d336b0a
Ohadgips <138259832+Ohadgips@users.noreply.github.com>
2024-02-04 09:06:54 +0200

    PE Behavior Analysis Part 1

--- Behavior_based_Detection/Behavior_Analysis.py
+++ Behavior_based_Detection/Behavior_Analysis.py
@@ -1,3 +1,35 @@
-import os
+import os,requests
 
 # use sandbox to analyze file behavior:
+API_URL = "https://localhost:8090/cuckoo/api"
+
+# choose a file to test
+def choose_file(file_path):
+    files = {"file":open(file_path,'rb')}
+
+    response = requests.post(f"{API_URL}/tasks/create/file",files=files)
+    
+    task_id = response.json.get('task_id')
+
+    return task_id
+
+# Get cuckoo sandbox analysis result
+def get_result(task_id):
+
+    response = requests.get(f"{API_URL}/tasks/report/{task_id}")
+    return response.json()
+
+
+if __name__ == "__main__":
+
+    # give file path
+    file_path = ""
+
+    # Start analysis
+    task_id = choose_file(file_path)
+    print ("ID: ",task_id)
+
+
+    # Get result
+    results = get_result(task_id)
+    print("results: ", results)
