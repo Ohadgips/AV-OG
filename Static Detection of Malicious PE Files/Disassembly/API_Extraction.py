319e4bbe1a96f693058f41f08d3aeece5ecb551b
Ohadgips <138259832+Ohadgips@users.noreply.github.com>
2024-03-23 22:30:20 +0200

    API_Extraction

--- Static Detection of Malicious PE Files/Disassembly/API_Extraction.py
+++ Static Detection of Malicious PE Files/Disassembly/API_Extraction.py
@@ -1,36 +1,39 @@
-import pefile,re
-def demangle_cpp_name(mangled_name):
-    # Example of regex to demangle MSVC++ decorated names
-    regex = r"\?(?P<func_name>[^\@@]+)@"
-    match = re.match(regex, mangled_name)
-    if match:
-        return match.group("func_name")
-    return mangled_name
+import pefile,re,cpp_demangle
+
+def decode_mangled_cpp(to_decode):
+    to_decode_str = to_decode.decode('utf-8')
+    print(to_decode_str)
+    decoded_symbol = cpp_demangle.demangled(to_decode_str)
+    print(type(decoded_symbol))
+    return decoded_symbol
+    
+# Check for mangled C++ names
+def is_mangled_cpp_name(function_name):
+    function_name_str = function_name.decode('utf-8')
+    return any(char in function_name_str for char in ['?', '$', '@'])
 
 def get_imported_functions(pe_file_path):
     pe = pefile.PE(pe_file_path)
+    pe.parse_data_directories()
 
     imported_functions = set()
 
     # Iterate through imported DLLs
     for entry in pe.DIRECTORY_ENTRY_IMPORT:
         dll_name = entry.dll.decode("utf-8")
-        
         # Iterate through imported functions from each DLL
         for imp in entry.imports:
-            imported_functions.add((dll_name, imp.name.decode("utf-8") if imp.name else "Ordinal_" + str(imp.ordinal)))
+            name = imp.name
+            if is_mangled_cpp_name(imp.name) == False:
+                name = imp.name.decode("utf-8")
+                imported_functions.add((dll_name,name))
 
     return imported_functions
 
 if __name__ == "__main__":
     pe_file_path = "C:\\Users\\USER\\Documents\\Audacity\\audacity.exe"
     imported_functions = get_imported_functions(pe_file_path)
-    print("Imported API Functions:")
-    for api_function in imported_functions:
-        print(api_function[1])
-        if str(api_function[1]).startswith("??"):  # Check if it's a C++ mangled name
-            api_function = list(api_function)
-            api_function[1] = demangle_cpp_name(api_function[1])
-        print(api_function)
-    #for dll, function in imported_functions:
-     #   print(f"{dll}: {function}")
+    for imp in imported_functions:
+        print(imp[1]+",")
+
+    
