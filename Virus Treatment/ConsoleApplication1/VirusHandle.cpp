395de9998f2677ce54fad605911f680528b05541
Ohadgips <138259832+Ohadgips@users.noreply.github.com>
2024-05-01 08:19:51 +0300

    Virus Handle Part 1

--- Virus Treatment/ConsoleApplication1/VirusHandle.cpp
+++ Virus Treatment/ConsoleApplication1/VirusHandle.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 #include <windows.h>
 #include <Shlwapi.h>
 #include <string>
@@ -13,29 +15,92 @@ struct QuarantinedFile {
     bool isClean; 
 };
 
+bool SetFolderPermissions(const wchar_t* folderPath) {
+    return true;
+}
+
+
+
+bool createFolder() {
+
+    std::string quarantineFolderPath = "Quarantine";
+
+    if (!filesystem::exists(quarantineFolderPath)) {
+        if (!filesystem::create_directory(quarantineFolderPath)) {
+            std::cerr << "error\n";
+            return false;
+        }
+        
+        // write a unique identifier 
+        std::string lockFilePath = quarantineFolderPath + "/access_lock.txt";
+        std::ofstream lockFile(lockFilePath);
+        if (lockFile.is_open()) {
+            lockFile << "da2541@gah&#fa23"; // the unique identifier
+            lockFile.close();
+            std::cout << "folder created and locked.\n";
+        }
+        else {
+            std::cerr << "error\n";
+            return false;
+        }
+    }
+    else {
+        cout << "Quarantine folder already exists.\n";
+    }
+    return true;
+}
+bool AccessQuarantineFolder() {
+    string quarantineFolderPath = "Quarantine";
+    string lockFilePath = quarantineFolderPath + "/access_lock.txt";
+    string expectedCode = "da2541@gah&#fa23";
+
+    if (std::filesystem::exists(lockFilePath)) {
+        std::ifstream lockFile(lockFilePath);
+        if (lockFile.is_open()) {
+            std::string content;
+            std::getline(lockFile, content);
+            lockFile.close();
+
+
+            if (content == expectedCode) {
+                cout << "can access folder\n";
+                return true; 
+            }
+        }
+    }
+    cout << "can NOT access folder\n";
+    return false; 
+}
+
 bool quarantineFile(const wchar_t* filePath) {
+
+
     wchar_t quarantineDir[MAX_PATH];
     wcscpy_s(quarantineDir, MAX_PATH, L"C:\\Quarantine");
 
     // creating the quarantine dir
-    if (!CreateDirectory(quarantineDir, NULL) && GetLastError() != ERROR_ALREADY_EXISTS) {
-        wcout << L"Error creating quarantine directory." << endl;
+    if (createFolder() == false)
+    {
+        wcout << L"Error creating directory." << endl;
         return false;
     }
-    wchar_t fileName[MAX_PATH];
-    wcscpy_s(fileName, MAX_PATH, PathFindFileName(filePath));
-    
-    // create path of the quarantined file
-    wchar_t quarantinedFilePath[MAX_PATH];
-    PathCombine(quarantinedFilePath, quarantineDir, fileName);
+    if (AccessQuarantineFolder()) {
+        wchar_t fileName[MAX_PATH];
+        wcscpy_s(fileName, MAX_PATH, PathFindFileName(filePath));
 
-    if (!MoveFile(filePath, quarantinedFilePath)) {
-        wcout << L"Error moving file to quarantine." << endl;
-        return false;
+        // create path of the quarantined file
+        wchar_t quarantinedFilePath[MAX_PATH];
+        PathCombine(quarantinedFilePath, quarantineDir, fileName);
+
+        if (!MoveFile(filePath, quarantinedFilePath)) {
+            wcout << L"Error moving file to quarantine." << endl;
+            return false;
+        }
+
+        wcout << L"File quarantined successfully: " << quarantinedFilePath << endl;
+        return true;
     }
 
-    wcout << L"File quarantined successfully: " << quarantinedFilePath << endl;
-    return true;
 }
 
 wstring stringToWChar(const std::string& str)
@@ -64,7 +129,7 @@ wstring stringToWChar(const std::string& str)
 
 void quarantine(string filePath)
 {
-    wstring wideString = stringToWChar(filePath); // Specify file path
+    wstring wideString = stringToWChar(filePath);
     wcout << wideString << endl;
     
     if (quarantineFile(const_cast<LPWSTR>(wideString.c_str()))) {
